5800de225b6940394cc8ce930d600af463362827
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-06-14 20:14:18 +0800

    /

---
+++ secret-base-login/loginscr.js
@@ -0,0 +1,6 @@
+const LoginButton = document.getElementById('login-button')
+const LoginResult = document.getElementById('login-result')
+LoginButton.onclick = () => {
+    const UserNameRE = /[0-9A-Za-z@_\-\.]+/
+    const SecureTokenSegRE = /[0-9A-Fa-f]+/
+}

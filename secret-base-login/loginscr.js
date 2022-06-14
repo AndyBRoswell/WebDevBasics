7c8e6358f5a6eda3a60f387eecaf83eff758a198
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-06-14 21:19:22 +0800

    Update loginscr.js

--- secret-base-login/loginscr.js
+++ secret-base-login/loginscr.js
@@ -1,11 +1,21 @@
 const LoginButton = document.getElementById('login-button')
 const LoginResult = document.getElementById('login-result')
-const Username = document.getElementById('username').value
+const UsernameInput = document.getElementById('username')
+
 LoginButton.onclick = () => {
     LoginResult.innerHTML = ''
+    const Username = UsernameInput.value
+    CheckUsername(Username)
+    CheckSecureToken()
+}
+
+function CheckUsername(Username) {
     const UsernameRE = /^[0-9A-Za-z@_\-\.]+$/
-    const SecTokenSegRE = /^[0-9A-Fa-f]+$/
     if (UsernameRE.test(Username) === false) {
-        LoginResult.innerHTML = `用户名的长度必须是 1 至 32 位，且仅允许包含：阿拉伯数字、大小写英文字母、4个特殊字符@_-.`
+        LoginResult.innerHTML += `用户名的长度必须是 1 至 32 位，且仅允许包含：阿拉伯数字、大小写英文字母、4个特殊字符@_-.`
     }
+}
+
+function CheckSecureToken(Token) {
+    const SecTokenRE = /^[0-9A-Fa-f]+$/
 }

7c5ae9b98f69e9721f0baf340fb5d5b71f2c77de
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2022-07-19 13:51:02 +0800

    Update script.js

--- screen-flasher/script.js
+++ screen-flasher/script.js
@@ -1,8 +1,18 @@
 const body = document.body
-window.addEventListener('click', () => {
+
+demo();
+
+async function demo() {
+    const delay = 250;
     while (true) {
         body.style.backgroundColor = '#000000'
+        await sleep(delay)
         body.style.backgroundColor = '#FFFFFF'
+        await sleep(delay)
         console.log('Loop executed.')
     }
-})
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
